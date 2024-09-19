754305190fbd1df55d24b159c54da9e1bc8baad3
alorslouis <alorslouis@users.noreply.github.com>
2024-09-19 15:51:01 -0700

    fix(middleware): narrow scope of checks
    only check the backend cookie
    
    secure? not really! but sufficient for this takehome...

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,20 @@
 import { defineMiddleware } from "astro:middleware";
 
-
 export const onRequest = defineMiddleware((context, next) => {
-
 
 	if (context.url.pathname === "/login") {
 		return next()
 	}
 
 	const fetchAccessToken = context.cookies?.get("fetch-access-token")
-	const AWSALB = context.cookies?.get("AWSALB")
-	const AWSALBCORS = context.cookies?.get("AWSALBCORS")
+	//const AWSALB = context.cookies?.get("AWSALB")
+	//const AWSALBCORS = context.cookies?.get("AWSALBCORS")
 
-	if (!fetchAccessToken || !AWSALB || !AWSALBCORS) {
+	//if (fetchAccessToken && AWSALB && AWSALBCORS) {
+	if (fetchAccessToken) {
+		context.locals.loggedIn = true
+	} else {
+		context.locals.loggedIn = false
 		return context.redirect("/login")
 	}
 
