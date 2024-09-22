5ffd1e028e69cb9eece927efdc6c53d5fcfeee25
alorslouis <alorslouis@users.noreply.github.com>
2024-09-21 19:19:34 -0700

    feat(zodTypes): (+) various parsers/types, API + stashing

--- src/appTypes/apiTypes.ts
+++ src/appTypes/apiTypes.ts
@@ -1,15 +1,65 @@
 import { z } from "zod"
 
+
 export const dogParser = z.object({
 	id: z.string(),
 	img: z.string(),
 	name: z.string(),
 	age: z.number(),
-	zip_code: z.string(),
 	breed: z.string(),
+	zip_code: z.string(),
 })
 
 export type Dog = z.infer<typeof dogParser>
+
+export const dogWithFavorite = dogParser.extend({
+	isFavorite: z.boolean()
+})
+
+export type DogWithFavorite = z.infer<typeof dogWithFavorite>
+
+export const dogBreedsParser = z.string().array()
+
+export type DogBreeds = z.infer<typeof dogBreedsParser>
+
+export const sortDirection = z.enum(["asc", "desc"])
+
+export type SortDir = z.infer<typeof sortDirection>
+
+export const sortableSearchParams = z.object({
+	breed: z.string(),
+	name: z.string(),
+	age: z.number(),
+})
+
+export const dogSearchParamsBase = z.object({
+	breeds: z.string().array().optional(),
+	zipCodes: z.string().array().optional(),
+	ageMin: z.number().optional(),
+	ageMax: z.number().optional(),
+	size: z.number().optional(),
+	from: z.number().optional(),
+})
+
+export const sortableSearchKeys = sortableSearchParams.keyof()
+
+export type SortableSearchKeys = z.infer<typeof sortableSearchKeys>
+
+export const sortSchema = z.custom<`${z.infer<typeof sortableSearchKeys>}:${z.infer<typeof sortDirection>}`>()
+
+export const dogSearchParams = dogSearchParamsBase.extend({
+	sort: sortSchema.optional()
+})
+
+export type DogSearchParams = z.infer<typeof dogSearchParams>
+
+export const dogSearchBody = z.string().array()
+
+export const dogSearchFetchParser = z.object({
+	next: z.string(),
+	resultIds: z.string().array(),
+	total: z.number()
+})
 
 export const locationParser = z.object({
 	zip_code: z.string(),
@@ -36,7 +86,20 @@ export const authLoginParser = z.object({
 
 export type AuthLogin = z.infer<typeof authLoginParser>
 
+export const formatAuthLoginParser = authLoginParser.transform((data) => {
+	return `name:${data.name}:email:${data.email}`;
+});
+
+
 export const authCookieParser = z.object({
 	cookieName: z.string(),
+	cookieValue: z.string(),
+	expires: z.date().optional(),
+	sameSite: z.union([z.boolean(), z.enum(["lax", "strict", "none"])]).optional(),
+	path: z.string().optional(),
+	httpOnly: z.boolean(),
+	secure: z.boolean(),
 
 })
+
+export type AuthCookie = z.infer<typeof authCookieParser>
