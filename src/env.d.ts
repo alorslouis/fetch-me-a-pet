e2a629c22c7857f815ab32ea702c17c0bf807210
alorslouis <alorslouis@users.noreply.github.com>
2024-09-21 19:16:21 -0700

    refactor(env.d.ts): userObject @ locals

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
+type AuthLogin = import("@typedef/apiTypes").AuthLogin
 
 declare namespace App {
 	interface Locals extends Runtime {
-		loggedIn: boolean
+		loggedIn: boolean,
+		userObject?: AuthLogin
 	}
 }
