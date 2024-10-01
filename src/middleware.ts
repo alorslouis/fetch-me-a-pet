1c554e3e4045fdfab60f2f5f400b26909afa07bf
alorslouis <alorslouis@users.noreply.github.com>
2024-10-01 11:28:29 -0700

    refactor(middleware): _images !== protect; init state functions
    +: _images to non-protected (header svg)
    ~: init functions, promise.all

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,19 @@
 import { defineMiddleware } from "astro:middleware";
 import { userStore } from "src/store/user";
 import { breedsStore } from "src/store/allBreeds";
-import { fetchFetch } from "./utils/fetchFetch";
-import { dogBreedsParser } from "@typedef/apiTypes";
 import { $dogFavorites } from "@stores/favorites";
 import { actions } from "astro:actions"
 import { $viewedDog } from "@stores/seenDog";
+import type { APIContext } from "astro";
+import { $matches } from "@stores/matches";
 
 export const onRequest = defineMiddleware(async (context, next) => {
 
-	if (context.url.pathname === "/login") {
+
+	const { pathname } = context.url
+
+	// INFO: handle for login routes and image files (sourced via _image path)
+	if (pathname === "/login" || pathname.includes("_image")) {
 		return next()
 	}
 
@@ -20,77 +24,21 @@ export const onRequest = defineMiddleware(async (context, next) => {
 
 		const user = userStore.get();
 
-		const breeds = breedsStore.get()
-		const favorites = $dogFavorites.get()
-		const viewed = $viewedDog.get()
-
-
-		// INFO: get then ssr/client cache the breeds on init...
-		// its ~static, so saves hitting constantly
-		if (!breeds) {
-			const getBreeds = await fetchFetch(context, {
-				path: "/dogs/breeds",
-				method: "GET"
-			})
-
-			const breeds = dogBreedsParser.parse(await getBreeds.json())
-
-			console.log({ breeds })
-
-			breedsStore.set(breeds)
-		}
-
-		// INFO: this gets insantiated in /login post route (SSR)
-		// set in locals so we can access in /_actions easily
-		// and handle for cookie is still active but nanostores cache is stale/wiped
-		// neat solve for locals (is per-route)
-		// but we'd go for a more roubst/secure solution for a *real* product (JWT, etc)
 		if (user) {
 			context.locals.userObject = user
-
-
-
-
-			// INFO: hit KV for prev results
-			// login clears local state
-
-			if (Object.keys(favorites).length === 0) {
-				console.log("no favorites!")
-				const { data, error } = await context.callAction(actions.listKvs.list, {})
-
-				if (error) console.error(error)
-
-				if (data) {
-					console.log("fetched favorites", { data })
-					data.kvMetadata.forEach(f => $dogFavorites.setKey(f.id, f))
-				} else {
-					$dogFavorites.set({})
-				}
-			}
-
-			if (Object.keys(viewed).length === 0) {
-				console.log("no previously viewed!")
-				const { data, error } = await context.callAction(actions.viewedDog.listViewed, {})
-
-				if (error) console.error(error)
-
-				if (data) {
-					console.log("fetched favorites", { data })
-					console.log({ viewedData: data })
-					data.kvMetadata.forEach(f => $viewedDog.setKey(f.id, f))
-				} else {
-					$viewedDog.set({})
-				}
-			} else {
-				console.log("no length on previously viewed!")
-				console.log({ viewedKeys: Object.keys(viewed) })
-			}
-
 		} else {
 			return context.redirect("/login")
 		}
 
-		console.log({ layoutUser: user });
+		const { breedsData, favoritesData, viewedData, matchesData } = await fetchLatestData(context)
+
+		//console.log({ breedsData, favoritesData, viewedData })
+
+		breedsStore.set(breedsData)
+		$dogFavorites.set(favoritesData)
+		$viewedDog.set(viewedData)
+		$matches.set(matchesData)
+
 	} else {
 		context.locals.loggedIn = false
 		return context.redirect("/login")
@@ -99,3 +47,65 @@ export const onRequest = defineMiddleware(async (context, next) => {
 	return next();
 })
 
+
+async function fetchLatestData(context: APIContext) {
+	const breedsPromise = fetchBreeds(context)
+	const favoritesPromise = fetchFavorites(context)
+	const viewedPromise = fetchViewed(context)
+	const matchesPromise = fetchMatches(context)
+
+	const [breedsData, favoritesData, viewedData, matchesData] = await Promise.all([
+		breedsPromise,
+		favoritesPromise,
+		viewedPromise,
+		matchesPromise
+	])
+
+	return { breedsData, favoritesData, viewedData, matchesData }
+}
+
+async function fetchBreeds(context: APIContext) {
+	const { data, error } = await context.callAction(actions.getDogs.getBreeds, {})
+
+	if (error) {
+		console.error(error)
+		return null
+	}
+
+	return data.breeds
+}
+
+async function fetchFavorites(context: APIContext) {
+	const { data, error } = await context.callAction(actions.listKvs.list, {})
+
+	if (error) {
+		console.error(error)
+		return {}
+	}
+
+	return data ? Object.fromEntries(data.kvMetadata.map(f => [f.id, f])) : {}
+}
+
+async function fetchViewed(context: APIContext) {
+	const { data, error } = await context.callAction(actions.viewedDog.listViewed, {})
+
+	if (error) {
+		console.error(error)
+		return {}
+	}
+
+	return data ? Object.fromEntries(data.kvMetadata.map(f => [f.id, f])) : {}
+}
+
+async function fetchMatches(context: APIContext) {
+	const { data, error } = await context.callAction(actions.matches.listMatches, {})
+
+	if (error) {
+		console.error(error)
+		return {}
+	}
+
+	//console.log({ fetchMatchTestData: data })
+
+	return data.matches ?? {}
+}
