4f86a0c7ed33359cb9bc38bb50c2597895931105
alorslouis <alorslouis@users.noreply.github.com>
2024-10-01 11:36:06 -0700

    feat(types): zod parsers + types

--- src/appTypes/apiTypes.ts
+++ src/appTypes/apiTypes.ts
@@ -1,6 +1,6 @@
 import { z } from "zod"
 
-
+// base Dog interface returned from API
 export const dogParser = z.object({
 	id: z.string(),
 	img: z.string(),
@@ -12,25 +12,70 @@ export const dogParser = z.object({
 
 export type Dog = z.infer<typeof dogParser>
 
+// extend with our isFavorite
 export const dogWithFavorite = dogParser.extend({
 	isFavorite: z.boolean()
 })
 
 export type DogWithFavorite = z.infer<typeof dogWithFavorite>
 
+// coerce (form passes as string)
+export const coercedDogWithFavorite = dogWithFavorite.extend({
+	age: z.coerce.number(),
+	isFavorite: z.preprocess((val) => val === 'true', z.boolean()).default(false)
+})
+
+export const dogSearchBody = z.string().array()
+
+// API returns array of strings for search
 export const dogBreedsParser = z.string().array()
 
 export type DogBreeds = z.infer<typeof dogBreedsParser>
 
-export const sortDirection = z.enum(["asc", "desc"])
-
-export type SortDir = z.infer<typeof sortDirection>
-
-export const sortableSearchParams = z.object({
-	breed: z.string(),
-	name: z.string(),
-	age: z.number(),
+export const dogSearchFetchParser = z.object({
+	next: z.string(),
+	resultIds: dogBreedsParser,
+	total: z.number()
 })
+
+export const searchExtras = z.object({
+	total: z.number(),
+	//cursor: z.number()
+})
+
+export type SearchExtras = z.infer<typeof searchExtras>
+
+// match KV
+// stored as stringified JSON
+
+export const matchKvMetadata = dogParser.extend({
+	matchId: z.string()
+})
+
+export type MatchKvMetadata = z.infer<typeof matchKvMetadata>;
+
+export const matchParser = z.object({
+	matchedDog: matchKvMetadata,
+	matchInputs: dogParser.array()
+})
+
+export type Match = z.infer<typeof matchParser>;
+
+export const viewedKvMetadata = dogParser.extend({
+	viewedAt: z.date()
+})
+
+export type ViewedKvMetadata = z.infer<typeof viewedKvMetadata>;
+
+export const parseViewedKvMetadata = dogParser.extend({
+	viewedAt: z.coerce.date()
+})
+//
+// helper for columns
+// omitting id (!relevant for UX)
+export const dogColumns = dogWithFavorite.omit({ id: true }).keyof();
+
+export type DogColumns = z.infer<typeof dogColumns>;
 
 export const dogSearchParamsBase = z.object({
 	breeds: z.string().array().optional(),
@@ -41,24 +86,47 @@ export const dogSearchParamsBase = z.object({
 	from: z.number().optional(),
 })
 
+export const sortableSearchParams = z.object({
+	breed: z.string(),
+	name: z.string(),
+	age: z.number(),
+})
+
+// sort direction + term
+// we submit as sep. form vals
+// then construct using z.custom
+export const sortDirection = z.enum(["asc", "desc"])
+
+export type SortDir = z.infer<typeof sortDirection>
+
 export const sortableSearchKeys = sortableSearchParams.keyof()
 
 export type SortableSearchKeys = z.infer<typeof sortableSearchKeys>
 
+// constructor for sortingBy
 export const sortSchema = z.custom<`${z.infer<typeof sortableSearchKeys>}:${z.infer<typeof sortDirection>}`>()
 
+// solve for edge case
+// z.coerce.number() transforms "" to 0
+// which is not what we want
+const formNumberPipe = z.union([
+	z.string().transform(val => val === "" ? undefined : Number(val)),
+	z.number()
+]).optional()
+
+// coerce for form parsing
 export const dogSearchParams = dogSearchParamsBase.extend({
-	sort: sortSchema.optional()
+	sort: sortSchema,
+	lockedBreeds: dogBreedsParser.optional(),
+	ageMin: formNumberPipe,
+	ageMax: formNumberPipe,
+	size: z.coerce.number().optional(),
 })
 
 export type DogSearchParams = z.infer<typeof dogSearchParams>
 
-export const dogSearchBody = z.string().array()
-
-export const dogSearchFetchParser = z.object({
-	next: z.string(),
-	resultIds: z.string().array(),
-	total: z.number()
+export const nextSearchParamParser = z.object({
+	from: z.coerce.number().gte(0),
 })
 
 export const locationParser = z.object({
@@ -78,6 +146,16 @@ export const coordinatesParser = z.object({
 })
 
 export type Coordinates = z.infer<typeof coordinatesParser>
+
+export const cloudflareGeoParser = z.object({
+	lat: z.coerce.number(),
+	long: z.coerce.number(),
+	city: z.string(),
+	state: z.string(),
+	zip: z.string(),
+})
+
+export type CloudflareGeo = z.infer<typeof cloudflareGeoParser>
 
 export const authLoginParser = z.object({
 	name: z.string(),
@@ -103,3 +181,15 @@ export const authCookieParser = z.object({
 })
 
 export type AuthCookie = z.infer<typeof authCookieParser>
+
+export const uspsZipLookupParser = z.object({
+	resultStatus: z.string(),
+	zip5: z.string(),
+	defaultCity: z.string(),
+	defaultState: z.string(),
+	defaultRecordType: z.string(),
+	citiesList: z.array(z.object({ city: z.string(), state: z.string() })),
+	nonAcceptList: z.array(z.object({ city: z.string(), state: z.string() }))
+})
+
+export type UspsZipLookupParser = z.infer<typeof uspsZipLookupParser>
